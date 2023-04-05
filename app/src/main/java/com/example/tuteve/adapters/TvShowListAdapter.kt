bd0a35dd997e1e38c1d628ec38fe82bd1dbfaf8e
davida2901 <davidadon29@gmail.com>
2023-04-05 16:55:22 -0400

    Fixed margin for item layout and added the image.

--- app/src/main/java/com/example/tuteve/adapters/TvShowListAdapter.kt
+++ app/src/main/java/com/example/tuteve/adapters/TvShowListAdapter.kt
@@ -3,10 +3,12 @@ package com.example.tuteve.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tuteve.R
 import com.example.tuteve.models.TVShowSummary
+import com.squareup.picasso.Picasso
 
 class TvShowListAdapter(val items : List<TVShowSummary>): RecyclerView.Adapter<TvShowListAdapter.ViewHolder>() {
 
@@ -15,6 +17,7 @@ class TvShowListAdapter(val items : List<TVShowSummary>): RecyclerView.Adapter<T
         val tvShowName = view.findViewById<TextView>(R.id.tvshow_name)
         val tvshowStartDate = view.findViewById<TextView>(R.id.tvshow_start_date)
         val tvShowNetwork = view.findViewById<TextView>(R.id.tvshow_network)
+        val tvShowImage = view.findViewById<ImageView>(R.id.tvShow_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -31,5 +34,13 @@ class TvShowListAdapter(val items : List<TVShowSummary>): RecyclerView.Adapter<T
         holder.tvShowName.text = tvShow.name
         holder.tvshowStartDate.text = tvShow.startDate
         holder.tvShowNetwork.text = tvShow.network
+
+        Picasso.get()
+            .load(tvShow.imageURL)
+            .resize(270,400)
+            .centerCrop()
+            .into(holder.tvShowImage)
+
+        holder.tvShowImage
     }
 }
